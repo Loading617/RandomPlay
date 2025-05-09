67bdcb0aef07d08dfb8203f4514f65fd1b4bc4d3
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-05-09 18:17:03 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("RandomPlay Extension Installed!");
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url.includes('youtube.com')) {
       chrome.scripting.executeScript({
